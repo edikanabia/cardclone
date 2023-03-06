ebfccbaf75eb026baa2d3e66e962acdd8e7940ac
edikanabia <49695530+edikanabia@users.noreply.github.com>
2023-03-05 22:13:07 -0500

    gamemanager got really bloated, confusing. started over. having a bit of a moment here (/neg).

--- cardclone/Assets/Scripts/Card.cs
+++ cardclone/Assets/Scripts/Card.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] bool isFaceUp = false;
+    //set type of card
     public enum CardType
     {
         Rock,
         Paper,
         Scissors
     }
-
     public CardType rpsType;
+
+    //sprites
     SpriteRenderer cardRenderer;
     public List<Sprite> sprites;
-    
+    public bool isFaceUp = false;
+
+    //movement
+    public Transform targetPos;
+    public bool moving;
+
+    //states
+    public bool playerCard = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,19 +36,36 @@ public class Card : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        #region card display logic
         //displays card face
-        if (isFaceUp)
+        if (!isFaceUp)
         {
-            cardRenderer.sprite = sprites[(int)rpsType];
+            Debug.Log("face down");
+            cardRenderer.sprite = sprites[3];
         }
         else
         {
-            cardRenderer.sprite = sprites[3];
+            cardRenderer.sprite = sprites[(int)rpsType];
         }
+        #endregion
+
+        //if (transform.position == targetPos.position)
+        //{
+        //    moving = false;
+        //}
+
+        transform.position = targetPos.position;
+
     }
 
     private void OnMouseDown()
     {
-        
+       //play card
+    }
+
+    private void OnMouseOver()
+    {
+        //hover
     }
 }
--- cardclone/Assets/Scripts/GameManager.cs
+++ cardclone/Assets/Scripts/GameManager.cs
@@ -1,73 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    //initialize info
-    int deckSize = 24;
-    public GameObject cardObject; //passes in card prefab
+    enum GameState
+    {
+        Shuffling,
+        Dealing,
+        Discarding,
+        Resetting
+    }
 
-    //scorekeeping
-    [SerializeField] int playerScore, opponentScore = 0;
-    TMP_Text playerScoreDisplay;
-    TMP_Text opponentScoreDisplay;
-
-
-
-    //keeping track of cards
-    [SerializeField] List<GameObject> cardDeck; //all cards on screen
-
-    
-
-
+    GameState gameState;
 
     // Start is called before the first frame update
     void Start()
     {
-        //adds rock to deck
-        for(int i = 0; i <deckSize/3; i++)
-        {
-            cardDeck.Add(cardObject);
-            Card card = cardDeck[i].GetComponent<Card>();
-            card.rpsType = Card.CardType.Rock;
-            Instantiate(cardDeck[i]);
-        }
-
-        //adds paper to deck
-        for (int i = 0; i < deckSize / 3; i++)
-        {
-            cardDeck.Add(cardObject);
-            Card card = cardDeck[i].GetComponent<Card>();
-            card.rpsType = Card.CardType.Paper;
-            Instantiate(cardDeck[i]);
-        }
         
-        //adds scissors to deck
-        for (int i = 0; i < deckSize / 3; i++)
-        {
-            cardDeck.Add(cardObject);
-            Card card = cardDeck[i].GetComponent<Card>();
-            card.rpsType = Card.CardType.Scissors;
-            Instantiate(cardDeck[i]);
-        }
-
-        playerScoreDisplay = GameObject.Find("Player Score").GetComponent<TMP_Text>();
-        opponentScoreDisplay = GameObject.Find("Opponent Score").GetComponent<TMP_Text>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerScoreDisplay.text = playerScore.ToString();
-        opponentScoreDisplay.text = opponentScore.ToString();
+        
     }
 
 
-    void ShuffleDeck()
-    {
-
-    }
 }
